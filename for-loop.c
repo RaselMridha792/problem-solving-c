d013bfe8d765f1690a5eb4416af794ffbdc4de00
RaselMridha792 <raselmridha792@gmail.com>
2025-04-29 21:03:14 +0600

    print 1 to n number sum using for loop

--- for-loop.c
+++ for-loop.c
@@ -6,8 +6,20 @@ int main () {
       // }
 
       // print 1 to 100 even number using for loop
-      for(int i = 0; i < 100; i+=2){
-            printf("%d\n", i);
-      }
+      // for(int i = 1; i < 100; i+=2){
+      //       printf("%d\n", i);
+      // }
+
+      // print 1 to n number. n is an input
+      // int n;
+      // printf("enter value of n: ");
+      // scanf("%d", &n);
+      // int sum = 0;
+      // for(int i = 1; i<=n; i++){
+      //       sum = sum + i;
+      // }
+      // printf("%d\n", sum);
+
+      
     return 0;
 }
