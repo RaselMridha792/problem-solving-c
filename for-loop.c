48acefbcd7c9c689024d3ba02a5bcfdaed4d578b
RaselMridha792 <raselmridha792@gmail.com>
2025-04-29 20:45:06 +0600

    print 1 to 100 even number using for loop

---
+++ for-loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+      // for(int i = 1; i < 100; i++){
+      //       printf("%d\n", i);
+      // }
+
+      // print 1 to 100 even number using for loop
+      for(int i = 0; i < 100; i+=2){
+            printf("%d\n", i);
+      }
+    return 0;
+}
